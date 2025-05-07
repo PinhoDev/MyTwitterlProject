fb99babc3098377c9a09e933d3076cab327659fe
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-07 20:07:03 +0200

    installed react-router-dom and setup some routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import ProfilePage from "./Pages/ProfilePage.jsx";
+import LogIn from "./Pages/LogIn";
+import SignUp from "./Pages/SignUp";
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+
+      <Routes>
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/Profile/:id" element={<ProfilePage />} />
+        <Route path="LogIn" element={<LogIn />} />
+        <Route path="SignUp" element={<SignUp />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
