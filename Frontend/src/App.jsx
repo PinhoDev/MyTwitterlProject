d792b66c084d682b790d8ac1cb6907cc3fa247e3
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-08 10:52:09 +0200

    Added Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import ProfilePage from "./Pages/ProfilePage.jsx";
 import LogIn from "./Pages/LogIn";
 import SignUp from "./Pages/SignUp";
+import Home from "./Pages/Home.jsx";
+
 function App() {
   return (
     <>
       <h1>Hello</h1>
 
       <Routes>
-        <Route path="/" element={<ProfilePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Profile/:id" element={<ProfilePage />} />
         <Route path="LogIn" element={<LogIn />} />
         <Route path="SignUp" element={<SignUp />} />
