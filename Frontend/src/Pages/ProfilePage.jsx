3ee85df7e1a955b2b519ca3145b5a488da709b2d
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-11 19:34:21 +0200

    First draft partial profilePage

--- Frontend/src/Pages/ProfilePage.jsx
+++ Frontend/src/Pages/ProfilePage.jsx
@@ -1,2 +1,57 @@
-function ProfilePage() {}
+import Arrow from "../assets/Arrow.png";
+import Beach from "../assets/DalmationBeachImg.png";
+import ProfilePic from "../assets/manPinkShirt.png";
+import "./working.css";
+//använde emojies medan jag arbetade - ska vi köra react-icons?
+function ProfilePage() {
+  const user = {
+    username: "daniel_feldman",
+    fullName: "Daniel Feldman",
+    email: "danieleldman@mail",
+    about: "Taking a short Twitter vacation",
+    occupation: "Astronomer",
+    location: "Stockholm",
+    website: "https://danielfeldman.space",
+    joinDate: "March 2021",
+    following: "10.2K",
+    followers: "13.8K",
+  };
+  return (
+    <>
+      <div className="profilePageContainer">
+        <div className="topBox">
+          <div className="arrowNameTweetsNumberBox">
+            <h2>
+              <img className="arrowImg" src={Arrow} alt="" /> Daniel feldman
+            </h2>
+            <p className="topBoxP">27.3K Tweets</p>
+          </div>
+
+          <div className="imgWrapper">
+            <img className="bannerImg" src={Beach} alt="Background image" />
+            <img className="avatarPic" src={ProfilePic} alt="Profile picture" />
+          </div>
+        </div>
+        <div className="formProfilePage" action="">
+          <div>
+            <h2> {user.fullName}</h2>
+            <p>{user.email}</p>
+            <p>{user.about}</p>
+            <p>💼{user.occupation}</p>
+            <p>🏠{user.location}</p>
+            <p>🔗{user.website}</p>
+            <p>🗓️{user.joinDate}</p>
+            <p>{user.following}</p>
+            <p> {user.followers}</p>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
 export default ProfilePage;
