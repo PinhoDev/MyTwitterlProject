a03ff8aee4a529e4482122b237de9dd09c8a6c1d
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-11 14:45:26 +0200

    Created frist draft LogIn page

--- Frontend/src/Pages/LogIn.jsx
+++ Frontend/src/Pages/LogIn.jsx
@@ -1,2 +1,49 @@
-function LogIn() {}
+import { Link, useNavigate, useState } from "react-router-dom";
+import Twitterbird from "../assets/Twitterbird.png";
+import "./working.css";
+
+function LogIn() {
+  const navigate = useNavigate();
+  const [formdata, setFormdata] = useState({
+    identifier: "",
+  });
+  const [error, setError] = useState();
+
+  const handleChange = (e) => {
+    setFormdata({ ...formdata, identifier: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formdata.identifier.trim() === "") {
+      setError("Fältet får inte vara tomt");
+    } else {
+      navigate("/loginpassword", {
+        state: { identifier: formdata.identifier },
+      }); //använder useLocation för att det är overkill med useContext när det bara ska flyttas från login till loginPassword och det inte går att använda props
+    }
+  };
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <img src={Twitterbird} alt="bild" />
+        <h1>Logga in på Twitter</h1>
+        <input
+          type="text"
+          value={formdata.identifier}
+          onChange={handleChange}
+          placeholder="Mobil, e-postadress eller användarnamn"
+        />
+        <p>
+          Har du inget konto?
+          <Link to="/signup">Registrera dig.</Link>
+        </p>
+        <button className="continue-button" type="submit">
+          Nästa
+        </button>
+      </form>
+    </>
+  );
+}
 export default LogIn;
