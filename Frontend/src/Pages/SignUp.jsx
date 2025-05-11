a3b7cea5eeee1dca8f3a7b2056aa94e6c4537aed
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-05-11 11:30:11 +0200

    Tog bort State längst upp i import

--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Auth.css";
 import image from "../Images/image.png";
