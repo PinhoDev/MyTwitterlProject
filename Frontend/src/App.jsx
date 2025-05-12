ac0d0ab4fcbee94f1363d56439502f666467808d
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-12 19:09:35 +0200

    no real change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import ProfilePage from "./Pages/ProfilePage.jsx";
 import LogIn from "./Pages/LogIn";
