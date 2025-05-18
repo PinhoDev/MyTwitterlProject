d4716c1949cbfb9714b9ce88d24ca14e84cb6cd0
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-18 17:48:18 +0200

    Added  alternative to createdAt becuse I saw the formatRelativeTime in utils

--- Frontend/src/Components/TweetCard.jsx
+++ Frontend/src/Components/TweetCard.jsx
@@ -1,8 +1,10 @@
+//import formatRelativeTime from "../utils/formatRelativeTime";
+
 function TweetCard({ tweet }) {
   //Formatera datum från ISO till något kort och läsbart
   function formatDate(isoString) {
     const date = new Date(isoString);
-    return date.toLocalDateString("sv-SE", {
+    return date.toLocaleDateString("sv-SE", {
       year: "numeric",
       month: "short",
       day: "numeric",
@@ -12,7 +14,8 @@ function TweetCard({ tweet }) {
     <div className="tweetCard">
       <div className="tweetHeader">
         <strong>{tweet.author.fullName}</strong> @{tweet.author.username}{" "}
-        {formatDate(tweet.createdAt)}
+        {formatDate(tweet.createdAt)}{" "}
+        {/*· {formatRelativeTime(tweet.createdAt)} */}
       </div>
       <div className="tweetContent">
         <p>{tweet.content}</p>
