800485366e0184634ddac280705faf556f238e29
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-18 18:01:40 +0200

    Used utils for hashtag

--- Frontend/src/Components/TweetCard.jsx
+++ Frontend/src/Components/TweetCard.jsx
@@ -1,24 +1,15 @@
-//import formatRelativeTime from "../utils/formatRelativeTime";
+import formatRelativeTime from "../utils/formatRelativeTime";
+import { formatTweetContent } from "../utils/formatTweetContent";
 
 function TweetCard({ tweet }) {
-  //Formatera datum från ISO till något kort och läsbart
-  function formatDate(isoString) {
-    const date = new Date(isoString);
-    return date.toLocaleDateString("sv-SE", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    });
-  }
   return (
     <div className="tweetCard">
       <div className="tweetHeader">
         <strong>{tweet.author.fullName}</strong> @{tweet.author.username}{" "}
-        {formatDate(tweet.createdAt)}{" "}
-        {/*· {formatRelativeTime(tweet.createdAt)} */}
+        {formatRelativeTime(tweet.createdAt)}
       </div>
       <div className="tweetContent">
-        <p>{tweet.content}</p>
+        <p>{formatTweetContent(tweet.content)}</p>
         {/*kommentar om vi vill*/}
       </div>
     </div>
