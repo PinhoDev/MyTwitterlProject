f2bbfa31f7d1900896e3c22bcf1e9f3ec9c24224
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-19 18:08:27 +0200

    Created functions in requestApi.js to get users own tweets and to get users friends tweets

--- Frontend/src/Model/requestApi.js
+++ Frontend/src/Model/requestApi.js
@@ -2,6 +2,9 @@ const BASE_URL = " http://127.0.0.1:3000/";
 const ENDPOINTS = {
   GET_USERNAME: `${BASE_URL}getUserName`,
   LOGIN: `${BASE_URL}login`,
+  GET_USERNAME: `${BASE_URL}login`,
+  GET_USER_TWEETS: (username) => `${BASE_URL}api/users/${username}`,
+  GET_FRIENDS_TWEETS: (userId) => `${BASE_URL}api/tweets/friends/${userId}`,
 };
 
 // Function to handle userName or email
@@ -52,5 +55,80 @@ export async function login(emailOrUsername, password) {
   } catch (error) {
     console.error("Error during login:", error);
     return { success: false, message: "An error occurred during login" };
+  }
+}
+
+//Function to get user details
+export async function getUserDetails(username) {
+  try {
+    const response = await fetch(`${BASE_URL}api/users/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const dataResponse = await response.json();
+
+    if (response.ok) {
+      return { success: true, userDetails: dataResponse.userDetails };
+    } else {
+      return { success: false, message: dataResponse.message };
+    }
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    return {
+      success: false,
+      message: "An error occurred while fetching user details",
+    };
+  }
+}
+//Function to get users own tweets
+
+export async function getUserOwnTweets(username) {
+  try {
+    const response = await fetch(ENDPOINTS.GET_USER_TWEETS(username), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok && data.success) {
+      return { success: true, tweets: data.tweets };
+    } else {
+      return { success: false, message: data.message };
+    }
+  } catch (error) {
+    console.error("Error fetching user's tweets:", error);
+    return {
+      success: false,
+      message: "Anerror occurred while fetching user's tweets",
+    };
+  }
+}
+
+//Get friends tweets
+export async function getFriendsTweets(userId) {
+  try {
+    const response = await fetch(ENDPOINTS.GET_FRIENDS_TWEETS(userId), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+
+    if (response.ok && data.success) {
+      return { success: true, tweets: data.tweets };
+    }
+  } catch (error) {
+    console.error("Error fetching friends' tweets:", error);
+    return {
+      success: false,
+      Message: "An error occurred while fetching friends' tweets",
+    };
   }
 }
