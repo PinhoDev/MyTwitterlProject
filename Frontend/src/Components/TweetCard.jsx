521b7bbb28713fcd3d903e9cca044f07faac9193
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-05-19 18:14:20 +0200

    Adjusted to fit structure

--- Frontend/src/Components/TweetCard.jsx
+++ Frontend/src/Components/TweetCard.jsx
@@ -1,11 +1,15 @@
-import formatRelativeTime from "../utils/formatRelativeTime";
+import { formatRelativeTime } from "../utils/formatRelativeTime";
 import { formatTweetContent } from "../utils/formatTweetContent";
+import { Link } from "react-router-dom";
 
 function TweetCard({ tweet }) {
   return (
     <div className="tweetCard">
       <div className="tweetHeader">
-        <strong>{tweet.author.fullName}</strong> @{tweet.author.username}{" "}
+        <strong>{tweet.author.fullName}</strong>{" "}
+        <Link to={`/profile/${tweet.author.username}`}>
+          @{tweet.author.username}
+        </Link>
         {formatRelativeTime(tweet.createdAt)}
       </div>
       <div className="tweetContent">
