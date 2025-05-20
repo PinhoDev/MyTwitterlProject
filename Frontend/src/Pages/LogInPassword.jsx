e0b355527943e9a0d782ebb4b5ac94e2706438af
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-05-20 12:00:37 +0200

    gjorde ändringar för att fixa css

--- Frontend/src/Pages/LogInPassword.jsx
+++ Frontend/src/Pages/LogInPassword.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./working.css";
+import "../styles/Auth.css";
 import Twitterbird from "../assets/Twitterbird.png";
+
 //lägg till error fel lösenoord
 function LogInPassword() {
   const [formdata, setFormdata] = useState({ password: "" });
@@ -29,13 +30,20 @@ function LogInPassword() {
         <form onSubmit={handleSubmit}>
           <img className="tweet-logo-login" src={Twitterbird} alt="bild" />
           <h2>Ange ditt lösenord</h2>
-          <input placeholder={identifier || "Användarnamn"} readOnly />
-          <input
-            type="password"
-            placeholder="Lösenord"
-            value={formdata.password}
-            onChange={handleAuthentication}
-          />
+          <div className="input-wrapper">
+            <input
+              className="login-input"
+              placeholder={identifier || "Användarnamn"}
+              readOnly
+            />
+            <input
+              className="login-input"
+              type="password"
+              placeholder="Lösenord"
+              value={formdata.password}
+              onChange={handleAuthentication}
+            />
+          </div>
           <button className="authbutton" type="submit">
             Logga in
           </button>
