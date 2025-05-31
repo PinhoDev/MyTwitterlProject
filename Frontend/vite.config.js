fcff462428b1cd8bc611c7c72b77089029491405
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-05-31 15:17:23 +0200

    Tagit bort profile i proxy för att inte kalla på den från backend och att man ska kunna refresha sidan utan att hamna på json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       "/users": "http://localhost:3000",
       "/tweets": "http://localhost:3000",
       "/login": "http://localhost:3000",
       "/register": "http://localhost:3000",
-      "/profile": "http://localhost:3000", // ✅ behövs
       "/follow": "http://localhost:3000",
     },
   },
